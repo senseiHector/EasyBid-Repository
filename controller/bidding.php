204945d64edc79dcf916e1156fe3c4c346534f1b
gedeonn <gedeoniyonkuru@gmail.com>
2017-04-21 21:26:13 +0000

    I though finishing my work earlier would be helpful for those doing other functionality. I am now 98% done with the item pages .
    Note that you will have to install my database that can be found in database/ Gedeon_data folder
    
    Thanks,

---
+++ controller/bidding.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *A controller page that gets informations about that the user need about the item
+ *@author Gedeon Niyonkuru
+ **/
+include("../database/dbcredentials.php");
+include("../database/dbconnection.php");
+
+//instantiating the database class
+ $database= new DBConnection();
+
+ //connecting to the database
+ $database->connect();
+
+ // making sure that the information gotten are secure and then querying the informations.
+ $id=$_POST['itid'];
+ $sql= "SELECT * FROM items WHERE item_id='$id'";
+$result=$database->query($sql)->fetch_assoc();
+$aid=$result['auctioneer_id'];
+$sql1= "SELECT username FROM users WHERE user_id='$aid'";
+$result1=$database->query($sql1)->fetch_assoc();
+array_push($result, $result1['username']);
+echo json_encode($result);
