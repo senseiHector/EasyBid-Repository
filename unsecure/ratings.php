0cfc64a20f97c7a6ccbd10c4307e5f5cf9741a18
Fran Antwi-Donkor <Fran Antwi-Donkor>
2017-04-23 10:27:48 +0100

    site performance code written

--- unsecure/ratings.php
+++ unsecure/ratings.php
@@ -17,12 +17,28 @@ function sellerPerformance(){
 		echo "Thank you for your comments";
 	}
 	else{
-		"Kindly rate this application and seller";
+		"Kindly rate this seller";
 	}
 
 }
 
 function sitePerformance(){
+	$rate = $_REQUEST['rate'];
+	$site = $_REQUEST['sitecomments'];
+	$statement = "INSERT INTO ratings(rating, comments) VALUES($rate, $site)";
+	
+	//connect to database
+	$db = new connect;
+
+	//execute the query
+	$rlt = $db->query($statement);
+
+	if($rlt){
+		echo "Thank you for your comments";
+	}
+	else{
+		"Kindly rate this seller";
+	}
 
 }
 ?>
