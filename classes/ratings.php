e8da58f0f32b7ae6369b46fb7df02a8b240b54af
Fran Antwi-Donkor <Fran Antwi-Donkor>
2017-04-23 18:33:37 +0100

    ratings validation

--- classes/ratings.php
+++ classes/ratings.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once("../database/dbconnection.php");
-
+$notice="";
 if(isset($_POST['rate'])){
 	sellerPerformance();
 	//sitePerformance();
@@ -19,45 +19,35 @@ function sellerPerformance(){
 	$site_rate = $_REQUEST['sitestarin'];
 	$site_comment = $_REQUEST['sitecomments'];
 	$statement = "INSERT INTO ratings (rating, comments,siteRating,siteComment) VALUES('$rate', '$comment','$site_rate','$site_comment')";
-if(empty($_POST['userstarin'])){
-	echo "Please rate us";
+if(empty($_POST['userstarin'] || $_POST['sitestarin'])){
+	$GLOBALS['notice'] = "Please rate us ";
+	
+}
+if(empty($_POST['usercomments'])|| $_POST['sitecomments']){
+	
+	$GLOBALS['notice'] .= "<br>" . "Your comment is highly valued, please add a comment";
+}
+else{
+	//connect to database
+	 $db = new dbconnection;
+
+	//execute the query
+	$rlt = $db->query($statement);
+
+	if($rlt>0){
+		echo "Thank you for your comments";
+	}
+	else{
+		echo "Kindly rate this seller";
+	}
+	$exec = $db->preStatement($rate, $comment);
+
 }
 	
-	//connect to database
-	//  $db = new dbconnection;
-
-	// //execute the query
-	// $rlt = $db->query($statement);
-
-	// if($rlt>0){
-	// 	echo "Thank you for your comments";
-	// }
-	// else{
-	// 	echo "Kindly rate this seller";
-	// }
-	// $exec = $db->preStatement($rate, $comment);
+	
  
 }
 
-// function sitePerformance(){
-// 	$site_rate = $_REQUEST['sitestarin'];
-// 	$site_comment = $_REQUEST['sitecomments'];
-// 	$statement = "INSERT INTO ratings(siteRating, siteComment) VALUES($site_rate, $site_comment)";
-	
-// 	//connect to database
-// 	$db = new dbconnection;
-
-// 	//execute the query
-// 	$rlt = $db->query($statement);
-
-// 	if($rlt){
-// 		echo "Thank you for your comments";
-// 	}
-// 	else{
-// 		"Kindly rate this seller";
-// 	}
-
-// }
 
 function ignore(){
 	echo "Thank you for using this application";
