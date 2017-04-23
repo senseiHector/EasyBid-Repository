32ad9ce4c7449973cfcc8006611ae317b4409820
gedeonn <gedeoniyonkuru@gmail.com>
2017-04-23 02:50:29 +0000

    Hector, the variables that I was passing through  are misbehaving  am still attempting to fix them

---
+++ controller/items.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ *A controller page that gets informations about the items and then display them to the user
+ *@author Gedeon Niyonkuru
+ **/
+include("../database/dbcredentials.php");
+include("../database/dbconnection.php");
+
+//instantiating the database class
+ $database= new DBConnection();
+ $db=$database->connect();
+ //connecting to the database
+ 
+
+
+function showitems($database)
+{
+    
+ // making sure that the information gotten are secure and then querying the informations.
+    $sql= "SELECT * FROM items";
+    $result=$database->query($sql)->fetch_all(MYSQLI_ASSOC);
+
+    $num=0;
+    foreach ($result as $r) {
+        $num++;
+        if ($num==6) {
+            echo "<div class='col-lg-2 col-md-3 col-sm-4 col-xs-6'>
+            <div class='thumbnail'>
+              <a href='bidding.php? s=".$r['icon_url']."' &id='".$r['item_id']."' > <img  src=".$r['icon_url']."> </a>
+              <div class='caption'>
+                <h3>".$r['item_name']."</h3>
+              </div>
+            </div>
+          </div>"."</div>"."<div class='row'>";
+        } else {
+            echo "<div class='col-lg-2 col-md-3 col-sm-4 col-xs-6'>
+            <div class='thumbnail'>
+              <a href='bidding.php? s='".$r['icon_url']."' &id='".$r['item_id']."' > <img  src=".$r['icon_url']."> </a>
+              <div class='caption'>
+                <h3>".$r['item_name']."</h3>
+              </div>
+            </div>
+          </div>";
+        }
+    }
+}
--- pages/items.php
+++ pages/items.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-
+<?php include('../controller/items.php');?>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -84,117 +84,7 @@
       </div>
       <div class="panel-body">
         <div class="row">
-          <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
-            <div class="thumbnail">
-              <a href="bidding.php? s=../image/items/1.jpg&id=1" ><img id="" class="img" src="../image/items/1.jpg"></a>
-              <div class="caption">
-                <h3>Ghanaian bag</h3>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
-            <div class="thumbnail">
-              <a href="bidding.php? s=../image/items/2.jpg&id=2"><img id="1" class="img" src="../image/items/2.jpg"></a>
-              <div class="caption">
-                <h3>Ghanaian T-SHIRT</h3>
-               
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
-            <div class="thumbnail">
-              <a href="bidding.php? s=../image/items/3.jpg&id=3 "><img id="2" class="img" src="../image/items/3.jpg"></a>
-              <div class="caption">
-                <h3>Ladies's beauty bag</h3>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
-            <div class="thumbnail">
-              <a href="bidding.php? s=../image/items/4.jpg&id=4 "><img id="3" class="img" src="../image/items/4.jpg"></a>
-              <div class="caption">
-                <h3>Ultimate perfume</h3>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
-            <div class="thumbnail">
-              <a href="bidding.php? s=../image/items/5.jpg&id=5 "><img id="4" class="img" src="../image/items/5.jpg"></a>
-              <div class="caption">
-                <h3>EON FLAM Bag</h3>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
-            <div class="thumbnail">
-              <a href="bidding.php? s=../image/items/6.jpg&id=6 "><img id="5" class="img" src="../image/items/6.jpg"></a>
-              <div class="caption">
-                <h3>HP_560</h3>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
-            <div class="thumbnail">
-              <a href="bidding.php? s=../image/items/7.jpg&id=7 "><img id="6" class="img" src="../image/items/7.jpg"></a>
-              <div class="caption">
-                <h3>HP_MONSTER</h3>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
-            <div class="thumbnail">
-              <a href="bidding.php? s=../image/items/8.jpg&id=8 "><img id="7" class="img" src="../image/items/8.jpg"></a>
-              <div class="caption">
-                <h3>II Phone 7 plus</h3>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
-            <div class="thumbnail">
-              <a href="bidding.php? s=../image/items/9.jpg&id=9 "><img id="8" class="img" src="../image/items/9.jpg"></a>
-              <div class="caption">
-                <h3>I phone 6</h3>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
-            <div class="thumbnail">
-              <a href="bidding.php? s=../image/items/10.jpg&id=10 "><img id="9" class="img" src="../image/items/10.jpg"></a>
-              <div class="caption">
-                <h3>Bikini</h3>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
-            <div class="thumbnail">
-              <a href="bidding.php? s=../image/items/11.jpg&id=11 "><img id="10" class="img" src="../image/items/11.jpg"></a>
-              <div class="caption">
-                <h3>Smart TV</h3>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
-            <div class="thumbnail">
-              <a href="bidding.php? s=../image/items/12.jpg&id=12 "><img id="11" class="img" src="../image/items/12.jpg"></a>
-              <div class="caption">
-                <h3>Sweet Jean</h3>
-              </div>
-            </div>
-          </div>
-        </div>
+            <?php showitems($database);?>
       </div>
       <div class="panel-footer">
         <div class="row">
