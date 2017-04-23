fea6e0a3ff45265d97fe2cc5410e61ea2a6cb53b
Hector Amoah <Sensei@Sensei-Mac.local>
2017-04-23 20:53:36 +0000

    Bidding Done

--- pages/paymentOptions.php
+++ pages/paymentOptions.php
@@ -12,39 +12,7 @@
 	<body style = "padding-top: 50px; padding-bottom: 150px;">
 	 <?php require_once("../controller/paymentoptionscontroller.php");?>
 	   <!--The navigation bar of the links-->
-		 <nav class="navbar navbar-default navbar-fixed-top">
-		 <div class="container-fluid">
-			 <!-- Brand and toggle get grouped for better mobile display -->
-			 <div class="navbar-header">
-				 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-								 data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-					 <span class="sr-only">Toggle navigation</span>
-					 <span class="icon-bar"></span>
-					 <span class="icon-bar"></span>
-					 <span class="icon-bar"></span>
-				 </button>
-				 <a class="navbar-brand" style = "padding:0" href="#">
-           <img src="../image/rsz_logo.png" alt="EasyBid Logo">
-         </a>
-			 </div>
-
-			 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				 <form class="navbar-form navbar-left">
-					 <div class="form-group">
-						 <input type="text" class="form-control" placeholder="Search">
-					 </div>
-					 <button type="submit" class="btn btn-default">Submit</button>
-				 </form>
-				 <ul class="nav navbar-nav navbar-right">
-					 <li><a href="home.php">Home</a></li>
-					 <li><a href="items.php">Buy</a></li>
-					 <li><a href="sellerUpload.php">Sell</a></li>
-					 <li><a href="account.php">Account</a></li>
-					 <li><a href="../index.php">Logout</a></li>
-				 </ul>
-			 </div>
-		 </div>
-		 </nav>
+		 <?php include("../layout/standardheader.php"); ?>
 
 		 <br><br>
 
--- pages/settings.php
+++ pages/settings.php
@@ -11,6 +11,7 @@
 	<link href="../css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body style = "padding-top: 50px; padding-bottom: 150px;">
+	<?php include("../layout/standardheader.php"); ?>
 	<div class = "container">
 		<div class = "row">
 			<div class="panel panel-default col-md-4 col-md-offset-4">
