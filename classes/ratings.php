267d8734136f377b6636703f7362130d25480b2e
Fran Antwi-Donkor <Fran Antwi-Donkor>
2017-04-23 23:20:27 +0100

    Comments added

--- classes/ratings.php
+++ classes/ratings.php
@@ -1,10 +1,23 @@
-<?php
 
+
+<?php
+/**
+* @author Frances Efua Antwiwah Antwi-Donkor
+* This class contains the php rating page used to input the stars and comments into the database
+**/
+
+/**
+* Database class is needed
+**/
 require_once("../database/dbconnection.php");
+
+/**
+* 
+**/
 $notice="";
 if(isset($_POST['rate'])){
 	sellerPerformance();
-	//sitePerformance();
+
 }
 
 if(isset($_POST['ignore'])){
@@ -12,13 +25,16 @@ if(isset($_POST['ignore'])){
 }
 
 function sellerPerformance(){
-	//header("Location: ../pages/wonbid.php");
-
+	
+	//variables are defined
 	$rate = $_POST['userstarin'];
 	$comment = $_REQUEST['usercomments'];
 	$site_rate = $_REQUEST['sitestarin'];
 	$site_comment = $_REQUEST['sitecomments'];
 	$statement = "INSERT INTO ratings (rating, comments,siteRating,siteComment) VALUES('$rate', '$comment','$site_rate','$site_comment')";
+	/**validation of the page
+	* These methods check if the stars and comments are empty
+	**/
 if(empty($_POST['userstarin'] || $_POST['sitestarin'])){
 	$GLOBALS['notice'] = "Please rate us ";
 	
@@ -34,6 +50,7 @@ else{
 	//execute the query
 	$rlt = $db->query($statement);
 
+	//check the result
 	if($rlt>0){
 		echo "Thank you for your comments";
 	}
@@ -41,14 +58,13 @@ else{
 		echo "Kindly rate this seller";
 	}
 
-
 }
-	
-	
  
 }
 
-
+/**
+* check if the user clicked no thanks
+**/
 function ignore(){
 	echo "Thank you for using this application";
 }
