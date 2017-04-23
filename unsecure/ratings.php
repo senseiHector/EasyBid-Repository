136d9779e7ef9e277fd7ea65ebcb63a893f43cdd
Fran Antwi-Donkor <Fran Antwi-Donkor>
2017-04-23 10:25:39 +0100

    seller performance code written

--- unsecure/ratings.php
+++ unsecure/ratings.php
@@ -3,9 +3,22 @@
 require_once("../databasse/dbconnection.php");
 
 function sellerPerformance(){
+	$rate = $_REQUEST['rate'];
+	$comment = $_REQUEST['usercomments'];
+	$statement = "INSERT INTO ratings(rating, comments) VALUES($rate, $comment)";
 	
 	//connect to database
 	$db = new connect;
+
+	//execute the query
+	$rlt = $db->query($statement);
+
+	if($rlt){
+		echo "Thank you for your comments";
+	}
+	else{
+		"Kindly rate this application and seller";
+	}
 
 }
 
